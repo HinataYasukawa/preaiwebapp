7c8514bceb52da2e28a84d2e7864390c9dc2ac9e
HinataYasukawa <h1i2n2a62208@gmail.com>
2024-11-01 23:22:38 +0900

    名前空間を追加

--- preai/file_upload/urls.py
+++ preai/file_upload/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views  # processingをviewsからインポート
 
+app_name = 'file_upload'  # 名前空間を追加
+
 urlpatterns = [
     path('', views.file_upload, name='file_upload'),
-    path('processing/<str:filename>/', views.processing, name='processing'),
+    path('processing/', views.processing, name='processing'),
 ]
--- preai/preai/urls.py
+++ preai/preai/urls.py
@@ -1,3 +1,5 @@
+# preai/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -5,6 +7,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('file_upload.urls')),
+    path('', include('file_upload.urls', namespace='file_upload')),  # namespaceを追加
     path('feedback/', include('feedback.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
